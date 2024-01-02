df678499e0a2e51e02bb6a9f0a67864ae8aa2960
Otto Martikainen <martikainenotto@gmail.com>
2024-01-02 10:22:10 +0200

    Find base sequences with convolution

--- README.md
+++ README.md
@@ -4,10 +4,18 @@
 
 This repo explores this idea and maybe later compares it to other solutions.
 
+## DNA base alignment
+
+I found out that DNA base sequences are aligned by using [Burrows-Wheeler Alignment](
+https://mr-easy.github.io/2019-12-19-burrows-wheeler-alignment-part-1/) could convolution be useful here, since it already gives us quality of match.
+
 ## References
 
 But what is a convolution? by 3Blue1Brown
 https://www.youtube.com/watch?v=KuXjwB4LzSA
 
-easy to understand 2D convolution implementation by @SamratSahoo
+Easy to understand 2D convolution implementation by @SamratSahoo
 https://gist.github.com/SamratSahoo/cef04a39a4033f7bec0299a10701eb95
+
+Burrows-Wheeler Alignment by Mr. Easy
+https://mr-easy.github.io/2019-12-19-burrows-wheeler-alignment-part-1/
--- conv-align.py
+++ conv-align.py
@@ -46,17 +46,21 @@ def convolve2D(image, kernel, padding=0, strides=1):
 
     return output
 
-def char_to_bits(char: int) -> list[int]:
-    return [(char >> i) & 1 for i in range(0, 8)]
+Bitfield = list[int]
+BASES: str = 'ACGT'
+BASE_BYTES: list[int] = [byte for byte in bytes(BASES, 'UTF-8')]
 
-def bits_to_char(bits: list[int]) -> int:
-    return sum([bits[i] * 2 ** i for i in range(0, 8)])
+def base_to_bits(byte: int) -> Bitfield:
+    index = BASE_BYTES.index(byte)
+    result = [0,0,0,0]
+    result[index] = 1
+    return result
 
 def str_to_one_hot(input: str) -> list[list[int]]:
-    return [char_to_bits(byte) for byte in bytes(input, 'UTF-8')]
+    return [base_to_bits(byte) for byte in bytes(input, 'UTF-8')]
 
 def one_hot_to_str(bits_lists: list[list[int]]) -> str:
-    return bytes([bits_to_char(bits) for bits in bits_lists]).decode('UTF-8')
+    return bytes([str_to_one_hot(bits) for bits in bits_lists]).decode('UTF-8')
 
 def reverse_hot(bits_lists: list[list[int]]) -> list[list[int]]:
     return [bits[::-1] for bits in bits_lists[::-1]]
@@ -64,10 +68,12 @@ def reverse_hot(bits_lists: list[list[int]]) -> list[list[int]]:
 def calculate_bits(bits_lists: list[list[int]]) -> int:
     return sum([sum(bits) for bits in bits_lists])
 
-def main_hot():
-    # Hypothesis, convolution can be used to find substring in text
-    haystack_str = 'How to Reverse a String in Python'
-    needle_len = random.randint(1, len(haystack_str) - 1)
+def main():
+    # can convolution used to find base alignment?
+    # https://mr-easy.github.io/2019-12-19-burrows-wheeler-alignment-part-1/
+    haystack_str = ''.join([random.choice(BASES) for _ in range(0, 1_000_000)])
+    max_needle_len = 40
+    needle_len = min(max_needle_len, random.randint(1, len(haystack_str) - 1))
     needle_index = random.randint(0, len(haystack_str) - needle_len)
     needle_str = haystack_str[needle_index:needle_index+needle_len]
     print(f'Expecting match at index {needle_index}, needle len {needle_len}')
@@ -75,14 +81,22 @@ def main_hot():
     haystack_hot = str_to_one_hot(haystack_str)
     needle_hot = reverse_hot(str_to_one_hot(needle_str))
     expected_bit_matches = calculate_bits(needle_hot)
+    close_bit_matches = expected_bit_matches * 0.55
 
     results = convolve2D(np.array(haystack_hot), np.array(needle_hot), 0)
 
     for index, bits_matching_np in enumerate(results):
         bits_matching = int(bits_matching_np[0])
         if bits_matching == expected_bit_matches:
-            print(f'Substring matched at index {index}')
-            print(f'Python index {haystack_str.index(needle_str)}')
+            print(f'Substring matched at index {index}, python {haystack_str.index(needle_str)}')
+            print(f'Reference: {haystack_str[index:index+needle_len]}')
+            print(f'Needle:    {needle_str}')
+            break
+        if bits_matching >= close_bit_matches:
+            print(f'Substring close match at index {index}')
+            print(f'Reference: {haystack_str[index:index+needle_len]}')
+            print(f'Needle:    {needle_str}')
+            print('')
 
 if __name__ == '__main__':
-    main_hot()
+    main()
